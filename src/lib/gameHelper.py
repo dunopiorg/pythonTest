4b6050dfcbbd81164be83829045f765e9f926095
Jang Hyun Woong <jhwskorg@gmail.com>
2017-11-29 17:02:10 +0900

    코드정리

--- src/lib/gameHelper.py
+++ src/lib/gameHelper.py
@@ -1,7 +1,6 @@
 from lib import DBConnection as db
 from lib import casterq
 from lib import record
-from enum import Enum
 from math import exp
 import korean
 import random
@@ -10,13 +9,13 @@ import time
 class GameHelper(object):
     # Setting Mysql Connector
     def __init__(self):
-        self.MySqlHost = 'localhost'
-        self.MySqlUser = 'root'
-        self.MySqlPw = 'lab2ai64'
-        self.MySqlDB = 'baseball'
-        self.MySqlPort = 3307
-        self.LiveData = None
-        self.CondTuple = None
+        self.HOST = 'localhost'
+        self.USER = 'root'
+        self.PASSWORD = 'lab2ai64'
+        self.DB = 'baseball'
+        self.PORT = 3307
+        self.live_data = None
+        self.event_cond_tuple = None
         self.curr_away_score = 0 # 원정팀 현재 점수
         self.curr_home_score = 0 # 홈팀 현재 점수
         self.prev_away_score = 0  # 원정팀 현재 점수
@@ -30,16 +29,25 @@ class GameHelper(object):
         self.get_catg_score()
         # TEST 를 위한 변수
         self.currRowNum = 0
+        self.get_event_catg()
 
+    # MSG Code 생성시 필요한 인자 정보 테이블
+    def get_event_catg(self):
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD,
+                                       db=self.DB)
+        query = "select mcode, event, param from baseball.event_catg"
+        self.event_catg_tuple = db_my_conn.select(query, False)
+
+    # Text이벤트에 따른 함수 활성화 정의 테이블
     def get_cond_dict(self):
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw, db=self.MySqlDB)
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD, db=self.DB)
         query = "select EVENT, CONDITIONS from baseball.event_cond"
-        self.CondTuple = db_my_conn.select(query, True)
+        self.event_cond_tuple = db_my_conn.select(query, True)
 
     # 이벤트 카테고리 별 점수
     def get_catg_score(self):
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw,
-                                       db=self.MySqlDB)
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD,
+                                       db=self.DB)
         query = "select category, score from baseball.catg_score"
         rows = db_my_conn.select(query, True)
         self.catg_score = {row["category"]: row["score"] for row in rows}
@@ -82,14 +90,14 @@ class GameHelper(object):
 
     # 실시간 데이터를 가져온다.
     def get_live_data(self):
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw, db=self.MySqlDB)
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD, db=self.DB)
         query = "select * from baseball.livetext_score_mix order by seqNo limit 1"
-        self.LiveData = db_my_conn.select(query, True)[0]
-        return self.LiveData
+        self.live_data = db_my_conn.select(query, True)[0]
+        return self.live_data
 
     # Live가 아닌 Local Test를 위한 함수. getLiveData()로 대체될 것.
     def test_live_data(self, gameid):
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw, db=self.MySqlDB)
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD, db=self.DB)
         query = "SELECT (SELECT NAME FROM baseball.person WHERE A.pitcher = PCODE) AS PITCHER_NAME "\
                        ", (SELECT NAME FROM baseball.person WHERE A.batter = PCODE) AS batter_name "\
                        ", (SELECT NAME FROM baseball.person WHERE A.catcher = PCODE) AS catcher_name "\
@@ -99,8 +107,8 @@ class GameHelper(object):
                        "WHERE A.gameID = '%s' "\
                        "ORDER BY seqNo" % gameid
 
-        self.LiveData = db_my_conn.select(query, True)
-        return self.LiveData
+        self.live_data = db_my_conn.select(query, True)
+        return self.live_data
 
     # 무엇을 할 지 결정한다.
     def get_what_info(self, livetext):
@@ -108,9 +116,9 @@ class GameHelper(object):
         result_condition = {}
         result_pitcher = {}
         result_hitter = {}
-        result_hitter_vs_pitcehr = {}
+        result_hitter_vs_pitcher = {}
 
-        live_data_dict = self.LiveData[self.currRowNum]
+        live_data_dict = self.live_data[self.currRowNum]
         curr_pitcher = live_data_dict['pitcher']
         curr_hitter = live_data_dict['batter']
 
@@ -118,7 +126,8 @@ class GameHelper(object):
         if self.prev_pitcher != curr_pitcher :
             if len(curr_pitcher) > 0:
                 self.prev_pitcher = curr_pitcher
-                pitcher_dict = self.get_df_season_pitcher_record(curr_pitcher) # 이번 시즌 투수 기록
+                # 이번 시즌 투수 기록
+                pitcher_dict = self.get_df_season_pitcher_record(curr_pitcher)
                 if pitcher_dict:
                     result_pitcher.update({'event_type': 'pitcher_season_record'})
                     result_pitcher.update(pitcher_dict)
@@ -136,16 +145,16 @@ class GameHelper(object):
                 # 이번 시즌 타자 vs 투수 기록
                 hitter_vs_pitcher_dict = self.get_df_season_bitter_vs_pitcher(curr_hitter, curr_pitcher)
                 if hitter_vs_pitcher_dict:
-                    result_hitter_vs_pitcehr.update({'event_type': 'hitter_on_mound'})
-                    result_hitter_vs_pitcehr.update(hitter_vs_pitcher_dict)
+                    result_hitter_vs_pitcher.update({'event_type': 'hitter_vs_pitcher'})
+                    result_hitter_vs_pitcher.update(hitter_vs_pitcher_dict)
 
         word_list = livetext.split()
 
         # 게임중 나타나는 이벤트 설정
-        for condDict in self.CondTuple:
-            if condDict['EVENT'] in word_list:
-                self.game_event = condDict['EVENT']
-                cond_list = condDict['CONDITIONS'].split(',')
+        for cond_dict in self.event_cond_tuple:
+            if cond_dict.get('EVENT') in word_list:
+                self.game_event = cond_dict.get('EVENT')
+                cond_list = cond_dict.get('CONDITIONS').split(',')
                 for cond in cond_list:
                     method = getattr(self, cond)
                     t_dict = method()
@@ -156,120 +165,57 @@ class GameHelper(object):
             result.update({'pitcher_info': result_pitcher})
         if result_hitter:
             result.update({'hitter_info': result_hitter})
-        if result_hitter_vs_pitcehr:
-            result.update({'hitter_vs_pitcher_info': result_hitter_vs_pitcehr})
+        if result_hitter_vs_pitcher:
+            result.update({'hitter_vs_pitcher_info': result_hitter_vs_pitcher})
         if result_condition:
-            result.update({'condition_info': result_condition})
+            result.update({'text_condition_info': result_condition})
 
         return result
 
+    # 상황별 문장을 만든다.
     def make_sentence(self, data):
-        class HREnum(Enum):
-            BASE_STATE, score_state, HR_CN, HR, RUN = range(1, 6)
-
-        class HMEnum(Enum):
-            RBI, BB, SB, H2, HIT, HR, KK, HRA  = range(1, 9)
-
-        class HVSPEnum(Enum):
-            HR_CN, HIT_CN, H2_CN, BB_CN = range(1, 5)
-
-        hr_dict = {'BASE_STATE': ['NAME', 'BASE_STATE'], 'HR_CN': ['PITCHER_NAME', 'HR_CN'], 'HR': ['NAME','HR'], 'RUN': ['RUN']}
-
         for key, value_dict in data.items():
-            msg_list = []
             event_type = value_dict['event_type']
+
             # 홈런 상황
-            if event_type == '홈런':
-                for hr_key, hr_values in hr_dict.items():
-                    if hr_key in value_dict:
-                        if value_dict[hr_key] is None:
-                            continue
-
-                        msg_code = 'HR%02dL0%02d' % (getattr(HREnum, hr_key).value, random.randrange(0, 2))
-                        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser,
-                                                       pw=self.MySqlPw,
-                                                       db=self.MySqlDB)
-                        query = "SELECT MSG FROM baseball.castermsg_mas WHERE CODE = '%s' limit 1" % msg_code
-                        template = db_my_conn.select(query, True)[0]
-
-                        msg_value = {}
-                        for v in hr_values:
-                            msg_value.update({v: value_dict[v]})
-
-                        msg = korean.l10n.Template(template['MSG']).format(**msg_value)
-                        msg_list.append([0.5, msg])
-                self.put_queue(msg_list)
-
-            elif event_type == '홈인':
-                print("캐스터: " + '홈인!')
+            if key == 'text_condition_info':
+                if event_type == '홈런':
+                    self.set_msg_value("HR", value_dict)
+                elif event_type == '홈인':
+                    print("캐스터: " + '홈인!')
 
             # 투수 등판 시
-            elif event_type == 'pitcher_season_record':
+            elif key == 'pitcher_info':
                 print("캐스터: ",  value_dict)
 
             # 타자 등판 시
-            elif event_type == 'hitter_on_mound':
-                # 타자 정보
-                if key is 'hitter_info':
-                    for hm_key, hm_value in value_dict.items():
-                        if hm_value == 0:
-                            continue
-
-                        if hm_key in HMEnum.__members__:
-                            msg_code = 'HM%02dL0%02d' % (getattr(HMEnum, hm_key).value, random.randrange(0, 2))
-                            db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort,
-                                                           user=self.MySqlUser,
-                                                           pw=self.MySqlPw,
-                                                           db=self.MySqlDB)
-                            query = "SELECT MSG FROM baseball.castermsg_mas WHERE CODE = '%s' limit 1" % msg_code
-                            template = db_my_conn.select(query, True)[0]
-
-                            msg = korean.l10n.Template(template['MSG']).format(**value_dict)
-                            value = self.score_generator(hm_key, value_dict)
-                            msg_list.append([value, msg])
-
-                    if len(msg_list) > 0:
-                        self.put_queue(msg_list)
+            elif key == 'hitter_info' or key == 'hitter_vs_pitcher_info':
+                if event_type == 'hitter_on_mound':
+                    self.set_msg_value("HM", value_dict)
 
                 # 타자와 투수 대결 정보
-                elif key is 'hitter_vs_pitcher_info':
-                    for hvsp_key, hvsp_value in value_dict.items():
-                        if hvsp_value == 0:
-                            continue
-
-                        if hvsp_key in HVSPEnum.__members__:
-                            msg_code = 'VS%02dL0%02d' % (getattr(HVSPEnum, hvsp_key).value, random.randrange(0, 1))
-                            db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort,
-                                                           user=self.MySqlUser,
-                                                           pw=self.MySqlPw,
-                                                           db=self.MySqlDB)
-                            query = "SELECT * FROM baseball.castermsg_mas WHERE CODE = '%s' limit 1" % msg_code
-                            template = db_my_conn.select(query, True)[0]
-
-                            msg = korean.l10n.Template(template['MSG']).format(**value_dict)
-                            value = self.score_generator(hvsp_key, value_dict)
-                            msg_list.append([value, msg])
-
-                    if len(msg_list) > 0:
-                        self.put_queue(msg_list)
+                elif event_type is 'hitter_vs_pitcher':
+                    self.set_msg_value("VS", value_dict)
 
                 # 주자 상황에 따른 타자 정보
-                elif key is 'on_base_info':
+                elif event_type is 'on_base_info':
                     print('주자 상황')
 
                 # 점수차 상황에 따른 타자 정보
-                elif key is 'score_gap_info':
+                elif event_type is 'score_gap_info':
                     print('점수차')
 
                 # 점수차와 주자 상황에 따른 타자 정보
-                elif key is 'score_base_info':
+                elif event_type is 'score_base_info':
                     print('점수차와 주자상황')
 
+    # Queue에 넣는다.
     def put_queue(self, msg):
         data = msg
         self.caster_queue.put(data)
         self.caster_queue.sort()
 
+    # Queue에서 뺀 후에 Decreasing한다. [Thread]
     def get_queue(self):
         while True:
             if self.caster_queue.size() > 0:
@@ -279,11 +225,10 @@ class GameHelper(object):
                     time.sleep(0.01*len(sentence))
                     self.caster_queue.decrease_q(0.01)
 
-
-    """ 이하 Activation Functions """
+    """ Start Activation Functions """
     # [HR]점수차 변화 : 도망(1), 추격(1), 동점(2), 리드(3), 역전(4)
     def get_score_status(self):
-        live_data_dict = self.LiveData[self.currRowNum]
+        live_data_dict = self.live_data[self.currRowNum]
         score_state = None
         score_state_name = None
 
@@ -327,7 +272,7 @@ class GameHelper(object):
     # [HR]주자 상황 : no, 1, 1-2or2-3, 1-2-3
     # return 1, 2, 3, 4
     def get_base_state(self):
-        live_data_dict = self.LiveData[self.currRowNum]
+        live_data_dict = self.live_data[self.currRowNum]
         count = 1
         base_list = ['base1', 'base2', 'base3']
 
@@ -338,8 +283,8 @@ class GameHelper(object):
 
     # [HR]리그 종류와 시즌에 대한 선수 기록
     def get_player_league(self):
-        live_data_dict = self.LiveData[self.currRowNum]
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw, db=self.MySqlDB)
+        live_data_dict = self.live_data[self.currRowNum]
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD, db=self.DB)
         query = "select * " \
                 " from baseball.battotal " \
                 "where PCODE = '%s' " \
@@ -363,8 +308,8 @@ class GameHelper(object):
 
     # WPA 정보 - currWPA(후), prevWPA(전), varWAP(변화)
     def get_wpa(self):
-        live_data_dict = self.LiveData[self.currRowNum]
-        db_my_conn = db.MySqlConnector(host=self.MySqlHost, port=self.MySqlPort, user=self.MySqlUser, pw=self.MySqlPw, db=self.MySqlDB)
+        live_data_dict = self.live_data[self.currRowNum]
+        db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT, user=self.USER, pw=self.PASSWORD, db=self.DB)
         query = "select BEFORE_WE_RT, AFTER_WE_RT, WPA_RT " \
                 " from baseball.ie_record_matrix " \
                 "where GAMEID = '%s' " \
@@ -377,22 +322,24 @@ class GameHelper(object):
 
     # [HR]상대선수와의 기록
     def get_vs_pitcher(self):
-        live_data_dict = self.LiveData[self.currRowNum]
+        live_data_dict = self.live_data[self.currRowNum]
         pitcher = live_data_dict['pitcher']
         hitter = live_data_dict['batter']
 
         return self.get_df_season_bitter_vs_pitcher(hitter, pitcher)
+    """ End Activation Functions """
 
+    """ Start Functions """
     # 메시지 점수 계산, list: 계산할 카테고리, dict: 해당 값
-    def score_generator(self, mkey, dict):
+    def score_generator(self, key_list, value_dict):
         rank_score = 0.1
-        m_key = mkey.split('_')[0]
+        catg_score_val = 0
 
-        if (m_key+'_RNK') in dict:
-            rank_score = round(0.5 - (dict.get(m_key+'_RNK') / 200), 3)
-
-        if mkey in self.catg_score:
-            catg_score_val = self.catg_score.get(mkey)
+        for key in key_list:
+            if key.find("_RNK") > 0:
+                rank_score = round(0.5 - (value_dict.get(key) / 200), 3)
+            elif key in self.catg_score:
+                catg_score_val = self.catg_score.get(key)
 
         result = round(self.get_new_sigmoid(rank_score + catg_score_val/2), 3)
 
@@ -402,3 +349,28 @@ class GameHelper(object):
     def get_new_sigmoid(self, x):
         return 1 / (0.667 + exp(-10*x + 5)) - 0.5
 
+    # DB에서 메시지 가져오고, Value 계산한 후에 Queue에 넣는다.
+    def set_msg_value(self, event_type, value_dict):
+        msg_list = []
+        event_tuple_list = [item for item in self.event_catg_tuple if item[1] == event_type]
+
+        for mcode, _, param in event_tuple_list:
+            p_list = param.split(',')
+            if all(item in value_dict for item in p_list):
+                if any(v for v in p_list if value_dict[v] == 0):
+                    continue
+                msg_code = "{}L0{:02d}".format(mcode, random.randrange(0, 2))
+                db_my_conn = db.MySqlConnector(host=self.HOST, port=self.PORT,
+                                               user=self.USER,
+                                               pw=self.PASSWORD,
+                                               db=self.DB)
+                query = "SELECT MSG FROM baseball.castermsg_mas WHERE CODE = '%s' limit 1" % msg_code
+                template = db_my_conn.select(query, True)[0]
+
+                msg = korean.l10n.Template(template['MSG']).format(**value_dict)
+                value = self.score_generator(p_list, value_dict)
+                msg_list.append([value, msg])
+
+        if msg_list:
+            self.put_queue(msg_list)
+    """ End Functions"""
