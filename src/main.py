1f7b16098fe9f0415acf88bf3e007df59d96c434
Ubuntu <ubuntu@ip-172-31-6-125.ap-northeast-2.compute.internal>
2018-05-03 07:11:29 +0000

    Thread 변경

--- src/main.py
+++ src/main.py
@@ -47,21 +47,26 @@ if __name__ == "__main__":
     except Exception as e:
         server.close()
     """
-    game_id = '20170609SSHH0'  # 20170914SKOB0  20170912OBNC0 20170926HHLT0
+    game_id = '20170914SKOB0'  # 20170609SSHH0  20170912OBNC0 20170926HHLT0
     sleep_second = config.SLEEP_TIME
     gm_app = game_app.GameApp(game_id)
 
     msg_thread = threading.Thread(target=gm_app.message_thread, name='Message Thread')
     msg_thread.start()
+    
     caster_thread = threading.Thread(target=gm_app.score_thread, name='Score Table Thread')
     caster_thread.start()
     # Test Start ------------------------------------
     game_live_tuple = gm_app.test_live_data(game_id)
+    gm_app.game_thread = 1
     for game_live_dict in game_live_tuple:
         result = gm_app.get_what_info(game_live_dict)
 
         if result:
             gm_app.make_sentence(result)
         time.sleep(sleep_second)
-
+    
+    gm_app.game_thread = 0
+    print("caster_thread isAlive():", caster_thread.is_alive())
+    print("msg_thread isAlive():", msg_thread.is_alive())
     # Test End -------------------------------------
